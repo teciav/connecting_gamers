14d158e2b28eea4f239d55ea760f2c225bb30e2d
Tecia Vailati <tecia.vailati@ontwerpwerk.nl>
2022-09-18 11:49:56 +0200

    BE: data base, requests

---
+++ server/src/utils/convert-minutes-to-hours-string.ts
@@ -0,0 +1,6 @@
+export function convertMinutesToHoursString(min: number) {
+    const hours = Math.floor(min / 60);
+    const minutes = min % 60;
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+}
